b7eacd10d2bff3bffd30043a23612037a339b0b9
Mary West <westmarye1219@gmail.com>
2020-02-22 11:23:12 -0600

    able to single view and view close button

--- main.js
+++ main.js
@@ -1,8 +1,58 @@
-const dinos = [];
+const dinos = [
+  {
+    id: 'dino1',
+    name: 'Rex',
+    type: 'T Rex',
+    age: 100,
+    owner: 'Zoe',
+    adventures: [],
+    health: 100,
+    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+  },
+
+  {
+    id: 'dino2',
+    name: 'Susan',
+    type: 'T Rex',
+    age: 100,
+    owner: 'Mary',
+    adventures: [],
+    health: 100,
+    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+  },
+
+  {
+    id: 'dino3',
+    name: 'Tim',
+    type: 'T Rex',
+    age: 100,
+    owner: 'Luke',
+    adventures: [],
+    health: 100,
+    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+  },
+
+];
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId, textToPrint)
   selectedDiv.innerHTML = textToPrint;
+}
+
+
+
+const viewSingleDino = () => {
+ let domString = ''
+ domString += '<button class="btn btn-danger">Close</button>'
+ printToDom('kennel', '')
+ printToDom('single-view', domString)
+}
+
+const singleDinoAddEvents = () => {
+  const dinoViewButtons = document.getElementsByClassName('single-dino');
+  for(let i = 0; i < dinoViewButtons.length; i++) {
+    dinoViewButtons[i].addEventListener('click', viewSingleDino);
+  }
 }
 
 const printDinos = (dinoArray) => {
@@ -14,11 +64,13 @@ const printDinos = (dinoArray) => {
     domString += '<div class="card-body">';
     domString += `  <h5 class="card-title">${dinoArray[i].name}</h5>`;
     domString += `  <p class="card-text">Health: ${dinoArray[i].health}</p>`;
+    domString += '<button class="btn btn-info single-dino">View</button>'
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';
   }
   printToDom('kennel', domString);
+  singleDinoAddEvents();
 };
 
 const newDino = (e) => {
@@ -43,6 +95,7 @@ const newDino = (e) => {
 
 const init = () => {
   document.getElementById('submit-new-dino').addEventListener('click', newDino)
+  printDinos(dinos)
 }
 
 init()
