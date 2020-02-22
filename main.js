92487f8f16b10a474ae762bdea4952eece9099cc
Mary West <westmarye1219@gmail.com>
2020-02-22 10:59:01 -0600

    dinos printing to the page

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const dinos = [];
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId, textToPrint)
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const printDinos = (dinoArray) => {
+  let domString = '';
+  for (let i =0; i < dinoArray.length; i++){
+    domString += '<div class="col-4">';
+    domString += '<div class="card">';
+    domString += `<img class="card-img-top" src="${dinoArray[i].imageUrl}" alt="Card image cap">`;
+    domString += '<div class="card-body">';
+    domString += `  <h5 class="card-title">${dinoArray[i].name}</h5>`;
+    domString += `  <p class="card-text">Health: ${dinoArray[i].health}</p>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
+  }
+  printToDom('kennel', domString);
+};
 
 const newDino = (e) => {
   e.preventDefault();
@@ -17,7 +37,7 @@ const newDino = (e) => {
   dinos.push(brandNewDino);
   document.getElementById('new-dino-form').reset();
   document.getElementById('collapseOne').classList.remove('show');
-  console.log('hi from new dino', dinos);
+  printDinos(dinos);
 };
 
 
