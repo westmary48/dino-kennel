393e48ba7329c44976cfb09662fd4b614a78db52
Mary West <westmarye1219@gmail.com>
2020-02-22 10:22:09 -0600

    reset the form

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('hi');
+const dinos = [];
+
+
+const newDino = (e) => {
+  e.preventDefault();
+
+  const brandNewDino = {
+  id: `dino${dinos.length +1}`,
+  name: document.getElementById('dino-name').value,
+  type: document.getElementById('dino-type').value,
+  age: document.getElementById('dino-age').value,
+  owner: document.getElementById('dino-owner').value,
+  adventures: [],
+  health: 100,
+  imageUrl: document.getElementById('dino-image').value,
+  }
+  dinos.push(brandNewDino);
+  document.getElementById('new-dino-form').reset()
+  console.log('hi from new dino', dinos);
+};
+
+
+const init = () => {
+  document.getElementById('submit-new-dino').addEventListener('click', newDino)
+}
+
+init()
